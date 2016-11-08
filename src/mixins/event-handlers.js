e76b30fcb159118c66911519f8c7a21be3b3518a
rickair <rickair@rickairdeMacBook-Air.local>
2016-11-08 17:05:00 +0800

    bug fix

--- src/mixins/event-handlers.js
+++ src/mixins/event-handlers.js
@@ -226,7 +226,7 @@ var EventHandlers = {
         return true;
       });
 
-      const slidesTraversed = Math.abs(swipedSlide.dataset.index - this.state.currentSlide) || 1;
+      const slidesTraversed = swipedSlide ? Math.abs(swipedSlide.dataset.index - this.state.currentSlide) || 1 : 1;
 
       return slidesTraversed;
     } else {
